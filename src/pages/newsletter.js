a843899d5a7da0c03e1a995184aa87bb9a90f084
Adria Castany <adria.castany@haufe.com>
2020-05-17 14:00:01 +0200

    Created required pages files

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Landing from '../sections/Landing';
+import Projects from '../sections/Projects';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const IndexPage = () => (
   <Layout>
     <Header />
-    <Landing />
+    <Projects />
     <Footer />
   </Layout>
 );
