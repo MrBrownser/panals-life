59ea78a782d368a8c67de7f87a9910d8a6812518
Adria Castany <adria.castany@haufe.com>
2020-05-17 22:16:18 +0200

    WIP finally displaying menu items on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react';
 import Headroom from 'react-headroom';
-import { Flex, Image } from 'rebass/styled-components';
 import styled from 'styled-components';
-import { SectionLinks } from 'react-scroll-section';
+import { Flex, Image } from 'rebass/styled-components';
 import Fade from 'react-reveal/Fade';
 import RouteLink from './RouteLink';
-import Logo from './Logo/Portfolio.svg';
-
-const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
+import Logo from './Logo/PanalsLife.svg';
 
 const HeaderContainer = styled(Headroom)`
   .headroom--pinned {
@@ -18,38 +15,54 @@ const HeaderContainer = styled(Headroom)`
   width: 100%;
 `;
 
-const formatLinks = (allLinks) =>
-  Object.entries(allLinks).reduce(
-    (acc, [key, value]) => {
-      const isHome = key === 'home';
-      return isHome
-        ? {
-            ...acc,
-            home: value,
-          }
-        : {
-            ...acc,
-            links: [...acc.links, { name: capitalize(key), value }],
-          };
+const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
+
+const menuItems = [
+  // Tienda
+  {
+    name: 'tienda',
+    value: {
+      onClick: (smth) => console.log('tienda onClick smth', smth),
+      isSelected: (smth) => console.log('tienda isSelected smth', smth),
     },
-    { links: [], home: null },
-  );
+  },
+  // Newsletter
+  {
+    name: 'newsletter',
+    value: {
+      onClick: (smth) => console.log('newsletter onClick smth', smth),
+      isSelected: (smth) => console.log('newsletter isSelected smth', smth),
+    },
+  },
+  // LOGO
+  {
+    name: 'home',
+    value: {
+      onClick: (smth) => console.log('home onClick smth', smth),
+      isSelected: (smth) => console.log('home isSelected smth', smth),
+    },
+  },
+  // Nativos
+  {
+    name: 'nativos',
+    value: {
+      onClick: (smth) => console.log('nativos onClick smth', smth),
+      isSelected: (smth) => console.log('nativos isSelected smth', smth),
+    },
+  },
+  // Contacta
+  {
+    name: 'contacta',
+    value: {
+      onClick: (smth) => console.log('contacta onClick smth', smth),
+      isSelected: (smth) => console.log('contacta isSelected smth', smth),
+    },
+  },
+];
 
-const Header = () => (
-  <HeaderContainer>
-    <Fade top>
-      <Flex
-        flexWrap="wrap"
-        justifyContent="space-between"
-        alignItems="center"
-        p={3}
-      >
-        <SectionLinks>
-          {({ allLinks }) => {
-            const { home, links } = formatLinks(allLinks);
-
-            const homeLink = home && (
-              <Image
+// For the HOME link we should put this one instead
+{
+  /* <Image
                 src={Logo}
                 width="50px"
                 alt="Portfolio Logo"
@@ -57,28 +70,40 @@ const Header = () => (
                 style={{
                   cursor: 'pointer',
                 }}
-              />
-            );
-            const navLinks = links.map(({ name, value }) => (
-              <RouteLink
-                key={name}
-                onClick={value.onClick}
-                selected={value.isSelected}
-                name={name}
-              />
-            ));
+              /> */
+}
 
-            return (
-              <Fragment>
-                {homeLink}
-                <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
-              </Fragment>
-            );
-          }}
-        </SectionLinks>
-      </Flex>
-    </Fade>
-  </HeaderContainer>
-);
+const Header = () => {
+  const navLinks = menuItems.map((item) => {
+    const { name, value } = item;
+    console.log('name', name);
+    console.log('value', value);
+    return (
+      <RouteLink
+        key={name}
+        onClick={value.onClick}
+        selected={value.isSelected}
+        name={name}
+      />
+    );
+  });
+
+  return (
+    <HeaderContainer>
+      <Fade top>
+        <Flex
+          flexWrap="wrap"
+          justifyContent="space-between"
+          alignItems="center"
+          p={3}
+        >
+          <Fragment>
+            <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
+          </Fragment>
+        </Flex>
+      </Fade>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
