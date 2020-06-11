d760b20991e6f25b6906c62083daff3f2978ea1a
Adria Castany <adria.castany@haufe.com>
2020-06-12 00:12:46 +0200

    Reduce array with all products by slug, keeping all combinations of colors/sizes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -15,6 +15,26 @@ const filterProducts = (nodes, selectedCategory) => {
   });
 };
 
+const groupProducts = (allProducts) => {
+  return allProducts.reduce((acc, currentItem) => {
+    if (acc.length === 0) return [currentItem];
+    const itemIndex = acc.findIndex((item) => item.slug === currentItem.slug);
+    if (itemIndex === -1) return [...acc, currentItem];
+    const addToThisItem = acc[itemIndex];
+    if (typeof addToThisItem.sizetypecolor === 'string') {
+      // We have to convert that string into an array of all colors
+      addToThisItem.sizetypecolor = [
+        addToThisItem.sizetypecolor,
+        currentItem.sizetypecolor,
+      ];
+      return acc;
+    }
+    // It's already an array
+    addToThisItem.sizetypecolor.push(currentItem.sizetypecolor);
+    return acc;
+  }, []);
+};
+
 const ProductList = ({ selectedCategory }) => (
   <StaticQuery
     query={graphql`
@@ -44,6 +64,8 @@ const ProductList = ({ selectedCategory }) => (
       const filteredProducts = !selectedCategory
         ? nodes
         : filterProducts(nodes, selectedCategory);
+
+      const groupedProducts = groupProducts(filteredProducts);
 
       return <div>{JSON.stringify(filteredProducts, null, 4)}</div>;
     }}
