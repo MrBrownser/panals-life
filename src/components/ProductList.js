de980218ce34a5edd8fd181bb5a889a5fbbd5b11
Adria Castany <adria.castany@haufe.com>
2020-06-12 02:24:04 +0200

    Temporary commit to show state

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,6 +3,49 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { StaticQuery, graphql } from 'gatsby';
 import { Box } from 'rebass/styled-components';
+import Fade from 'react-reveal/Fade';
+
+import { CardContainer, Card } from '../components/Card';
+
+// Imitar cómo crea el Post pero haciendo mi ProductCard
+// const Post = ({ title, text, image, url, date, time }) => (
+//   <a
+//     href={url}
+//     target="__blank"
+//     title={title}
+//     style={{ textDecoration: 'none' }}
+//   >
+//     <Card pb={4}>
+//       <EllipsisHeading m={3} p={1} color="text">
+//         {title}
+//       </EllipsisHeading>
+//       {image && <CoverImage src={image} height="200px" alt={title} />}
+//       <Text m={3} color="text">
+//         {text}
+//       </Text>
+//       <ImageSubtitle bg="primary" color="white" x="right" y="bottom" round>
+//         {`${date} - ${Math.ceil(time)} min`}
+//       </ImageSubtitle>
+//     </Card>
+//   </a>
+// );
+
+// Post.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   text: PropTypes.string.isRequired,
+//   image: PropTypes.string.isRequired,
+//   url: PropTypes.string.isRequired,
+//   date: PropTypes.string.isRequired,
+//   time: PropTypes.number.isRequired,
+// };
+
+const ProductCard = (product) => {
+  return (
+    <Card>
+      <p>{JSON.stringify(product)}</p>
+    </Card>
+  );
+};
 
 const filterProducts = (nodes, selectedCategory) => {
   const { value: selectedCategoryId } = selectedCategory;
@@ -67,7 +110,17 @@ const ProductList = ({ selectedCategory }) => (
 
       const groupedProducts = groupProducts(filteredProducts);
 
-      return <div>{JSON.stringify(filteredProducts, null, 4)}</div>;
+      // return <div>{JSON.stringify(filteredProducts, null, 4)}</div>;
+
+      return (
+        <CardContainer minWidth="300px">
+          {groupedProducts.map((product) => (
+            <Fade bottom key={product.slug}>
+              <ProductCard product={product} />
+            </Fade>
+          ))}
+        </CardContainer>
+      );
     }}
   />
 );
